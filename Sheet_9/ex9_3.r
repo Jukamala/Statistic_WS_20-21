4033279fd2d67156df2c5a8d63b055ae090b10d4
Justus Will <justuswill@gmail.com>
2021-01-18 12:27:15 +0100

    small changes

--- Sheet_9/ex9_3.r
+++ Sheet_9/ex9_3.r
@@ -1,5 +1,5 @@
 std_test <- function(std1, std2, n1, n2, alpha){
-  F = std1^2 / std2^2
+  F = std2^2 / std1^2
   q = qf(1 - alpha/2, n1-1, n2-1)
   
   cat(sprintf("F = %.2f, quantile = %.2f\n", F, q))
--- Sheet_9/ex9_4.r
+++ Sheet_9/ex9_4.r
@@ -2,7 +2,7 @@
 library("compositions")
 
 gaus_test <- function(M, N, alpha){
-  # frequencies of false rejection
+  # frequencies of false rejections
   rel = c()
   for(n in N){
     # p-values
@@ -14,10 +14,16 @@ gaus_test <- function(M, N, alpha){
     rel = c(rel, mean(p < alpha))
   }
   plot(N, 100 * rel, type="l", xlab="N", ylab="False Rejections [ in %]", col="dodgerblue",
-       main=sprintf("Two-Sided Gaustest (alpha = %g)", alpha))
+       main=sprintf("Two-Sided Gaustest (alpha = %d %%)", 100 * alpha))
 }
+
+# Plot in one window
+par(mfrow=c(3, 1))
 
 Ns = seq(10, 200, 10)
 for(alpha in c(0.01, 0.5, 0.1)){
   gaus_test(500, Ns, alpha)
+  abline(h=100 * alpha, col="red")
 }
+
+# As expected, relative frequencies of false rejections are around alpha and independent of N.
